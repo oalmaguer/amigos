412c667761eae2086eaa80fa96c9722138cea1dc
oli <almaguero95@gmail.com>
2024-10-19 12:06:01 -0700

    fixed petid

--- app/adopta/[petId]/page.tsx
+++ app/adopta/[petId]/page.tsx
@@ -6,7 +6,8 @@ import { useParams } from "next/navigation";
 
 export default function Adopta() {
   const pets2: any = usePets();
-
+  const { petId }: any = useParams();
+  console.log(petId);
   console.log(pets2);
   if (pets2) {
     const pet = pets2.pets.find((pet: any) => pet.id === parseInt(petId));
